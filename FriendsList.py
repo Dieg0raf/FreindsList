0ed4499f539dd39a1b3d66b1f83fb48c8eee2c5b
Diego Rafael <rafael2002diego@gmail.com>
2022-06-21 23:51:24 -0700

    Completed 'if user enter 3'. Use length of list instead of just "List" in for loop on line 55.

--- FriendsList.py
+++ FriendsList.py
@@ -22,53 +22,77 @@ def print_friendsList(List):
 
 # function used to ouput the choice the user chose
 def menu_Option(userChoice, List):
+    # if user enter 1
     if(userChoice == 1):
         print('-'*30)
-        friend = input("Add friend name: ")
+        friend = input("Add friend name: ") # name of the new Friend that is going to be added to Friends List
         print()
-        List.append(friend)
+        List.append(friend) # Friend is appended to the end of the list
         print(f"\'{friend}\' has been added to your Friends List.")
         print()
+    # if user enters 2
     elif(userChoice == 2):
         print('-'*30)
-        deleteName = input("Delete Name: ")
+        deleteName = input("Delete Name: ") # name of Friend that you user wants to delete
         print()
-        List.remove(deleteName)
-        print(f"\'{deleteName}\' has been deleted from your Friends List.")
-        print()
-    
-    # come back to this problem !
-    elif(userChoice == 3):
-        changeName = input("Name of the friend you want to change: ")
-        newName = input("New Name: ")
         for i in range(len(List)):
-            if(list[i] == changeName):
-                list[i] = newName
-                print(f"\'{changeName}\'s name has been changed to \'{newName}\'.")
+            if(List[i] == deleteName):
+                List.remove(deleteName)
+                print(f"\'{deleteName}\' has been deleted from your Friends List.") # if Friend is found it prints this message
+                notThere = True
                 print()
                 break
+            else:
+                notThere = False
+        if(notThere == False):
+            print(f"\'{deleteName}\' does not exist on your Friends List. (Try again)")
+    
+    # if user enters 3
+    elif(userChoice == 3):
+        changeName = input("Name of the friend you want to change: ") # name wanting to change
+        newName = input("New Name: ") # new name that is going to be given in place of the old name
+        # checks through list to find the name of the user
+        for i in range(len(List)):
+            if(List[i] == changeName):
+                List[i] = newName
+                print(f"\'{changeName}\'s name has been changed to \'{newName}\'.") # if found it will change and output this message
+                notThere = True
+                print()
+                break
+            else:
+                notThere = False
+        if(notThere == False):
+            print(f"\'{changeName}\' does not exist on your Friends List. (Try again)")
+            print() # if not found it will outpout this message and ask to retry
+    
+    # if user enters 4 
     elif(userChoice == 4):
         print('-'*30)
-        searchName = input("Search Name: ")
+        searchName = input("Search Name: ") # name wanted to search in Friends List
         print()
+        # checks to see if name of Friend is or is not on the Friends List
         if searchName in List:
-            print(f"\'{searchName}\' is already on your Friends List.")
+            print(f"\'{searchName}\' is on your Friends List.")
         else:
             print(f"\'{searchName}\' is not on your Friends List.")
+    
+    # if user enter 5
     elif(userChoice == 5):
         print('-'*26)
-        print("Friends List")
+        print("Friends List") # Freinds list is outputed
         print('-'*26)
-        print_friendsList(List)
+        print_friendsList(List) # call function which prints out Friends List
+    
+    # if user enters 6
     elif(userChoice == 6):
         print('-'*26)
-        print("Friends List".center(26))
+        print("Friends List".center(26)) # Freinds List is outputed onto screen
         print('-'*26)
         if(len(List) == 0):
-            print("You have no Freinds LOL")
+            print("You have no Freinds LOL") # if no Friends this message outputs
             print()
         else:
-            print_friendsList()
+            print_friendsList(List)
         print("Thank you for making your Freinds List with my program")
 
 # Display of the starting screen
